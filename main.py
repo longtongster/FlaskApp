8d2c0719bc8f6644f3f59bbae60a96d3a77f9dde
longtongster <sacha.van.weeren@gmail.com>
2021-10-26 20:17:46 +0200

    Initial commit for flaskapp

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route('/', methods=["GET","POST"])
+def index():
+    return "<h1>Flask application is running</h1>"
+
+if __name__=="__main__":
+    application.run(debug=True)
